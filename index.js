850edee0a29b6ed44303c85735c042d9518d3575
JJatau <jjatau14@outlook.com>
2023-07-17 01:13:51 +0100

    Add responsive sticky hamburger menu

--- index.js
+++ index.js
@@ -2,6 +2,41 @@
 
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
+const navBar = document.querySelector(".navbar");
+const header = document.querySelector(".head");
+
+const body = document.body
+let lastScroll = 0;
+
+// window.addEventListener("scroll", () => {
+//   navBar.classList.toggle("sticky", window.scrollY > 0);
+//   navMenu.classList.remove("active", window.scrollY > 0);
+// })
+
+window.addEventListener("scroll", () => {
+  const currentScroll = document.documentElement.scrollTop;
+  if (currentScroll <= 0) {
+    body.classList.remove("scroll-up")
+  }
+
+  if (currentScroll > lastScroll && !body.classList.contains("scroll-down"))  {
+    body.classList.add("scroll-down")
+  }
+
+
+  if (currentScroll < lastScroll && body.classList.contains("scroll-down"))  {
+    body.classList.remove("scroll-down")
+  }
+
+  lastScroll = currentScroll
+
+})
+
+window.addEventListener("scroll", () => {
+  if (navMenu.classList.contains('active') && body.classList.contains('scroll-down')) {
+    body.classList.remove("scroll-down");
+  } 
+})
 
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
@@ -12,3 +47,4 @@ document.querySelectorAll(".nav-link").forEach(nav => nav.addEventListener("clic
   hamburger.classList.remove("active");
   navMenu.classList.remove("active");
 }))
+
