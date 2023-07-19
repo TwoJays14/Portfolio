f921420da82971969a396b265d8f99df04840b86
JJatau <jjatau14@outlook.com>
2023-07-19 20:43:21 +0100

    Animated background added to main section

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 const navBar = document.querySelector(".navbar");
 const header = document.querySelector(".head");
+const about = document.getElementById("about");
+const wrapper = document.querySelector(".wrapper");
+const projects = document.querySelector(".wrapper2");
+const contact = document.getElementById("contact");
+const contactSection = document.querySelector(".wrapper3");
+const main = document.querySelector(".main-container");
+
 
 const body = document.body
 let lastScroll = 0;
@@ -47,4 +54,34 @@ document.querySelectorAll(".nav-link").forEach(nav => nav.addEventListener("clic
   hamburger.classList.remove("active");
   navMenu.classList.remove("active");
 }))
+
+
+
+// Prevent link default behavior
+const scrollIntoView = document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+          behavior: 'smooth'
+      });
+  });
+});
+
+
+about.addEventListener("click", function() {
+  console.log("I have clicked about")
+  wrapper.scrollIntoView(true)
+  stickyNav()
+});
+
+about.addEventListener("click", function() {
+  wrapper.scrollIntoView(true)
+})
+
+contact.addEventListener("click", function() {
+  contactSection.scrollIntoView(true)
+
+});
+
 
